cd7763fe5edbf62f9778f6d8118a84d54e71a5c6
acebrian <acebrian@paradigmadigital.com>
2017-10-27 22:07:13 +0200

    Generic service

---
+++ src/main/java/com/opipo/ultimategamesrating/services/ServiceDTOInterface.java
@@ -0,0 +1,26 @@
+package com.opipo.ultimategamesrating.services;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public interface ServiceDTOInterface<T, ID extends Serializable> {
+    T find(ID id);
+
+    Collection<T> findAll();
+
+    T create();
+
+    T create(ID id);
+
+    T save(T element);
+
+    void delete(T element);
+
+    void delete(ID id);
+
+    Boolean exists(ID id);
+
+    void checkExists(ID id) throws IllegalArgumentException;
+
+    void checkDoesntExists(ID id) throws IllegalArgumentException;
+}
