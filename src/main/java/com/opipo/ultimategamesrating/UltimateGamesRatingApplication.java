f587d6f929fa695cdddb44ede3eef5ecf597383f
acebrian <acebrian@paradigmadigital.com>
2017-10-27 21:11:59 +0200

    Frist steps

---
+++ src/main/java/com/opipo/ultimategamesrating/UltimateGamesRatingApplication.java
@@ -0,0 +1,12 @@
+package com.opipo.ultimategamesrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateGamesRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateGamesRatingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/opipo/ultimategamesrating/UltimateGamesRatingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.opipo.ultimategamesrating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UltimateGamesRatingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
