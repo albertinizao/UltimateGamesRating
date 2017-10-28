d3b292b69b8ca3f4fd9e7fee6b1eb67fe10b9c94
acebrian <acebrian@paradigmadigital.com>
2017-10-28 12:18:29 +0200

    Generation controller

---
+++ src/main/java/com/opipo/ultimategamesrating/controller/GenerationController.java
@@ -0,0 +1,29 @@
+package com.opipo.ultimategamesrating.controller;
+
+import com.opipo.ultimategamesrating.model.Generation;
+import com.opipo.ultimategamesrating.service.GenerationService;
+import com.opipo.ultimategamesrating.service.ServiceDTOInterface;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/position")
+@Api(value = "REST API to manage the position of the duties")
+public class GenerationController extends AbstractCRUDController<Generation, String> {
+
+    @Autowired
+    private GenerationService service;
+
+    @Override
+    protected ServiceDTOInterface<Generation, String> getService() {
+        return service;
+    }
+
+    @Override
+    protected boolean checkIdFromElement(String id, Generation element) {
+        return (id == null && (element == null || element.getId() == null))
+                || (id != null && element != null && element.getId() != null && id.equals(element.getId()));
+    }
+}
