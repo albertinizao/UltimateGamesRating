2b05fce74904f16179e1af45ac0469d7553fabda
acebrian <acebrian@paradigmadigital.com>
2017-10-28 19:01:26 +0200

    refactor

--- src/main/java/com/opipo/ultimategamesrating/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opipo.ultimategamesrating.repository;
-
-import com.opipo.ultimategamesrating.model.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company, String> {
-}
--- src/main/java/com/opipo/ultimategamesrating/service/CompanyService.java
+++
@@ -1,6 +0,0 @@
-package com.opipo.ultimategamesrating.service;
-
-import com.opipo.ultimategamesrating.model.Company;
-
-public interface CompanyService extends ServiceDTOInterface<Company, String> {
-}
